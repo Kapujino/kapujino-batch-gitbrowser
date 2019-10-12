6fbec77756358c7998c82f0ff7b93b58955917d2
Kapujino <>
2019-10-12 16:10:39 +0200

    version 0.1.0

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd initial; zip ../kapujino_initial-gitbrowser.installer-0.1.0.zip manifest.xml configs/*; cd ..
+cd xstream_fix; zip ../kapujino_xstream_fix-gitbrowser.installer-0.1.0.zip manifest.xml configs/*; cd ..
+cd adult; zip ../kapujino_adult-gitbrowser.installer-0.1.0.zip manifest.xml; cd ..
