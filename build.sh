4e244e04964f424f7c76cb51f25e820920ae3648
Kapujino <>
2019-10-15 12:35:34 +0200

    make it more flexible

--- build.sh
+++ build.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
-cd initial; zip ../kapujino_initial-gitbrowser.installer-0.1.1.zip manifest.xml configs/*; cd ..
-cd xstream_fix; zip ../kapujino_xstream_fix-gitbrowser.installer-0.1.0.zip manifest.xml configs/*; cd ..
-cd adult; zip ../kapujino_adult-gitbrowser.installer-0.1.1.zip manifest.xml; cd ..
+[ $# -eq 0 ] && {
+	echo "ERROR: NO argument!"
+	echo "usage: ./build.sh [TYPE] [VERSION]"
+	echo "currently types: initial, xstream_fix, adult"
+	exit 1
+}
+[ -e $2] && {
+	echo "ERROR: VERSION argument missing!"
+	echo "usage: ./build.sh [TYPE] [VERSION]"
+	echo "currently types: initial, xstream_fix, adult"
+	exit 1
+}
+
+TYPE=$1
+VERSION=$2
+
+case "$TYPE" in
+	"initial")
+		cd initial; zip ../kapujino_initial-gitbrowser.installer-$VERSION.zip manifest.xml configs/*; cd ..
+		;;
+	"xstream_fix")
+		cd xstream_fix; zip ../kapujino_xstream_fix-gitbrowser.installer-$VERSION.zip manifest.xml configs/*; cd ..
+		;;
+	"adult")
+		cd adult; zip ../kapujino_adult-gitbrowser.installer-$VERSION.zip manifest.xml; cd ..
+		;;
+	*)
+		echo "usage: ./build.sh [TYPE] [VERSION]"
+		echo "available types: initial, xstream_fix, adult"
+		exit 1
+		;;
+esac
